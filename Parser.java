3228d7a5f2fd8c77ed81b8e1f3fb3c80f6b54ef5
esandine <esandine@stuy.edu>
2017-03-07 20:58:13 -0500

    Save and line functions work

--- Parser.java
+++ Parser.java
@@ -4,19 +4,32 @@ import java.io.FileNotFoundException;
 public class Parser{
     public static void readFile(String filename){
 	File f = new File(filename);
-	while(s.hasNextLine()){
-            System.out.println("ELY "+s.nextLine());
-        }
-
 	try{
 	    Scanner s = new Scanner(f);
+	    Grafix g = new Grafix();
+	    String command;
+	    Coor[] line;
+	    Scanner args;
+	    Pixel p = new Pixel(200,0,0);
+	    while(s.hasNextLine()){
+		command = s.nextLine();
+		if(command.equals("line")){
+		    args = new Scanner(s.nextLine());
+		    line = new Coor[2];
+		    line[0] = new Coor(args.nextInt(), args.nextInt(), args.nextInt());
+		    line[1] = new Coor(args.nextInt(), args.nextInt(), args.nextInt());
+		    g.addEdge(line);
+		}else if(command.equals("save")){
+		    g.writeCoors(p);
+		    g.write(s.nextLine());
+		}
+	    }	
 	}catch(FileNotFoundException e){
 	    System.out.println("NO FILE HATH BEN FOUND");
 	}
     }
     
     public static void main(String[] args){
-	System.out.println("We got us here a parser");
-	readFile("display.sh");
+	readFile("script");
     }
 }
